3f39f340b9de28866064d1b2622a5b9685085986
nece001 <nece001@163.com>
2025-10-05 21:04:52 +0800

    添加查询定义

---
+++ src/Database/IQuery.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Nece\Framework\Adapter\Contract\DataBase;
+
+/**
+ * 查询接口
+ *
+ * @author nece001@163.com
+ * @create 2025-10-05 19:46:58
+ */
+interface IQuery
+{
+    // 查询要实现的方法
+}
