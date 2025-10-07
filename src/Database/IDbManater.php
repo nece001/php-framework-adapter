f20570c144f9115c2669e5a061704dc7a0553867
nece001 <nece001@163.com>
2025-10-07 23:32:31 +0800

    添加方法

--- src/Database/IDbManater.php
+++ src/Database/IDbManater.php
@@ -34,6 +34,79 @@ interface IDbManater
     public static function raw($value);
 
     /**
+     * 原始函数表达式
+     *
+     * @author nece001@163.com
+     * @create 2025-10-07 23:04:25
+     *
+     * @param string $func 函数名
+     * @param string $field 字段名
+     * @param string $alias 别名
+     * @return Expression
+     */
+    public static function rawFunc(string $func, string $field, string $alias);
+
+    /**
+     * 计数函数表达式
+     *
+     * @author nece001@163.com
+     * @create 2025-10-07 23:04:43
+     *
+     * @param string $field 字段名
+     * @param string $alias 别名
+     * @return Expression
+     */
+    public static function rawCount(string $field, string $alias);
+
+    /**
+     * 求和函数表达式
+     *
+     * @author nece001@163.com
+     * @create 2025-10-07 23:04:52
+     *
+     * @param string $field 字段名
+     * @param string $alias 别名
+     * @return Expression
+     */
+    public static function rawSum(string $field, string $alias);
+
+    /**
+     * 平均值函数表达式
+     *
+     * @author nece001@163.com
+     * @create 2025-10-07 23:05:01
+     *
+     * @param string $field 字段名
+     * @param string $alias 别名
+     * @return Expression
+     */
+    public static function rawAvg(string $field, string $alias);
+
+    /**
+     * 最小值函数表达式
+     *
+     * @author nece001@163.com
+     * @create 2025-10-07 23:05:10
+     *
+     * @param string $field 字段名
+     * @param string $alias 别名
+     * @return Expression
+     */
+    public static function rawMin(string $field, string $alias);
+
+    /**
+     * 最大值函数表达式
+     *
+     * @author nece001@163.com
+     * @create 2025-10-07 23:05:19
+     *
+     * @param string $field 字段名
+     * @param string $alias 别名
+     * @return Expression
+     */
+    public static function rawMax(string $field, string $alias);
+
+    /**
      * 查询单条记录
      *
      * @param  string  $query
