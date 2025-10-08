d54b4f305a6558dad3b1f29e12a2f67170b94638
nece001 <nece001@163.com>
2025-10-08 11:49:00 +0800

    增加方法定义

--- src/Database/IModel.php
+++ src/Database/IModel.php
@@ -70,4 +70,37 @@ interface IModel
      * @return bool
      */
     public function delete();
+
+    /**
+     * 查询表
+     *
+     * @author nece001@163.com
+     * @create 2025-10-08 11:34:30
+     *
+     * @param string $table
+     * @param string $alias
+     * @return self
+     */
+    public function from(string $table, string $alias = '');
+
+    /**
+     * 获取表别名
+     *
+     * @author nece001@163.com
+     * @create 2025-10-08 11:34:30
+     *
+     * @return string
+     */
+    public function getAlias();
+
+    /**
+     * 填充字段数据
+     *
+     * @author nece001@163.com
+     * @create 2025-10-08 11:44:13
+     *
+     * @param array $data
+     * @return self
+     */
+    public function fillData(array $data): self;
 }
